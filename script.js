9529d8fbab3a1be50646c1479aae29697ee1ed59
Nobuhle Ngcobo <nobuhlengcobo2000@gmail.com>
2025-09-10 15:22:37 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Example: Smooth scrolling for navigation links
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function(e){
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
-});
